40575c785a38ae5a86efae2380bb39177197d83f
DisGuiser9 <2319474254@qq.com>
2024-03-04 01:42:13 +0800

    merge all dataset to train.json

--- transfer.py
+++ transfer.py
@@ -2,38 +2,43 @@ import os
 import json
 
 def transfer():
-    #Change directory to your dataset directory
-    directory = 'the root to your dataset directory'
+    # Change directory to your dataset directory
+    directory = 'root'
     filenames = os.listdir(directory)
 
-    #Automatically load JSON files
+    # Automatically load JSON files
     json_files = [filename for filename in filenames if filename.endswith(".json")]
-    if json_files is None:
+    if not json_files:
         raise IndexError
+
+    # Prepare a new_data list to hold all conversations
     new_data = []
+
     for json_file in json_files:
         json_file_path = os.path.join(directory, json_file)
         try:
             with open(json_file_path, 'r',encoding='utf-8') as f:
                 data = json.load(f)
-
+            
             for item in data:
                 if 'instruction' in item and 'output' in item:
-                # Create a new dictionary 
+                    # Append user and assistant's speech to the conversation
                     conversation = {"conversations": [
-                    {"role": "user", "content": item['instruction']},
-                    {"role": "assistant", "content": item['output']}
+                        {"role": "user", "content": item['instruction']},
+                        {"role": "assistant", "content": item['output']}
                     ]}
                     new_data.append(conversation)
                 else:
                     print(f"Key not found, skipping item.")
                     continue
-
-            with open('new_' + str(json_file), 'w', encoding='utf-8') as f:
-                # dump the file, set indent with 4, ensure_ascii=False to avoid error
-                json.dump(new_data, f, ensure_ascii=False, indent=4)
         except Exception as e:
             print(f"Error processing {json_file}: {e}")
 
-if __name__ == '__main__':
+    # Write all conversations into one file "train.json"
+    with open('train.json', 'w', encoding='utf-8') as f:
+        # dump the file, set indent with 4, ensure_ascii=False to avoid error
+        json.dump(new_data, f, ensure_ascii=False, indent=4)
+        f.write('\n')
+
+if __name__ == "__main__":
     transfer()
